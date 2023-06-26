8820298173049a1adfbcab9a7233ff1b36412b79
freestyle.06@mail.ru <freestyle.06@mail.ru>
2023-06-26 22:17:09 +0300

    al hosts ed

--- macsite/settings.py
+++ macsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gop)ry!%v5&tvx_9nku2o(q9f_qn$r1htnvp9l$f1*te@39e4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'oxydear.pythonanywhere.com']
 
 
 # Application definition
